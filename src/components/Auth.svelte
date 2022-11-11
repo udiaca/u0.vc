38dd1721dec3dea7854529cf2d5517f80814a68a
Alexander Wong <alex@udia.ca>
2022-11-11 16:44:26 -0700

    wip: webauthn navigator credentials request
    - make this occur on button click instead
    - fix local https, otherwise get operation is insecure error
    - need to update user payload to be derived from form/server lookup

--- src/components/Auth.svelte
+++ src/components/Auth.svelte
@@ -1,10 +1,58 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
   import { user } from '../utils/svelteStores'
+
+  export let randomStringFromServer: string
 
   let authOk = false
   let tosOk = false
+  $: navigatorCredentials = false;
+
+  onMount(() => {
+    navigatorCredentials = navigator && !!navigator.credentials;
+  })
+
+  let credentialPromise = new Promise(() => {});
+  $: {
+    // https://webauthn.guide/#webauthn-api
+    if (navigatorCredentials) {
+      credentialPromise = navigator.credentials.create({
+        publicKey: {
+          challenge: Uint8Array.from(randomStringFromServer, (c) =>
+            c.charCodeAt(0)
+          ),
+          rp: {
+            name: 'Udia Systems',
+            id: window.location.host,
+          },
+          user: {
+            id: Uint8Array.from('UZSL85T9AFC', (c) => c.charCodeAt(0)),
+            name: 'lee@webauthn.guide',
+            displayName: 'Lee',
+          },
+          pubKeyCredParams: [{ alg: -7, type: 'public-key' }],
+          authenticatorSelection: {
+            authenticatorAttachment: 'cross-platform',
+          },
+          timeout: 60000,
+          attestation: 'direct',
+        },
+      })
+    }
+  }
+
+
 </script>
 
+<h1>{navigatorCredentials}</h1>
+
+{#await credentialPromise}
+  <p>...waiting</p>
+{:then credential}
+  <p>{credential}</p>
+{:catch error}
+  <p>{error}</p>
+{/await}
 {#if !$user}
   <form method="post" action="/api/auth/github/authorize" disabled={!authOk}>
     <div>
@@ -12,7 +60,10 @@
       <input id="ack-set-cookie" type="checkbox" bind:checked={tosOk} />
     </div>
     <div>
-      <label for="ack-tos">I agree to the <a href="/en/terms_of_service">Terms of Service</a>.</label>
+      <label for="ack-tos"
+        >I agree to the <a href="/en/terms_of_service">Terms of Service</a
+        >.</label
+      >
       <input id="ack-tos" type="checkbox" bind:checked={authOk} />
     </div>
     <input
